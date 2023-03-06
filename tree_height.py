5bf1dc32d55c66323b09d3d8e1dc852104a7c811
DenisBsl <98440192+DenisBsl@users.noreply.github.com>
2023-03-06 03:30:04 +0000

    auto grade upd.

--- tree_height.py
+++ tree_height.py
@@ -25,28 +25,23 @@ def compute_height(n, parents):
 
 
 def main():
-    data = input()
-    match data[0]:
-        case "F":
-            data = input()
-            with open(data) as f:
-                n = int(f.readline())
-                parents = list(map(int, f.readline().split(" ")))
-        case "I":
-            n = int(input())
-            parents = list(map(int, input().split(" ")))
-        case _:
-            return
-    
-    print(compute_height(n, parents))
+    input_type = input()
 
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    if 'I' in input_type:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        height = compute_height(n, parents)
+        print(height)
+    elif 'F' in input_type:
+        filename = input()
+        with open("test/" + filename, 'r') as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+            height = compute_height(n, parents)
+            print(height)
+    else:
+        print("Invalids")
+        exit()
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
